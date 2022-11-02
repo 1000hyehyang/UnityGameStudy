78a82af2bb0bf147f6bcc6faea78f6c5d7842c01
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2022-11-02 17:41:53 +0900

    Player(Rabbit)

--- 2.Rabbit RPG/Player.cs
+++ 2.Rabbit RPG/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     float x; //x축 입력값
     Rigidbody2D rigid;
     bool isOnGround;
+    bool isDamaged;
     Animator animator;
     SpriteRenderer sr;
     public AudioSource star;
@@ -18,27 +19,31 @@ public class Player : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-   
+
     void Update() //Input과 같은 것은 여기에 넣어주자
     {
-        x = Input.GetAxisRaw("Horizontal"); //방향키 입력
-
-        if(Input.GetKeyDown(KeyCode.Space)&& isOnGround) //점프
+        if(!isDamaged)  //이거 isDamaged로 안묶어주면 enemy에 맞고 튕길 때 y축으로만 뜀
         {
-            rigid.AddForce(Vector2.up * 7f, ForceMode2D.Impulse);
-            animator.SetTrigger("shouldJump");
-            isOnGround = false;
-            animator.SetBool("isOnGround", false);
+            x = Input.GetAxisRaw("Horizontal"); //방향키 입력
+
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround) //점프
+            {
+                rigid.AddForce(Vector2.up * 7f, ForceMode2D.Impulse);
+                animator.SetTrigger("shouldJump");
+                isOnGround = false;
+                animator.SetBool("isOnGround", false);
+            }
         }
+       
 
         //경사면에서 튕김 방지
-        if(isOnGround)
+        if (isOnGround)
         {
             if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
             {
                 rigid.velocity = new Vector2(rigid.velocity.x, -1.67f);
             }
-                    
+
         }
         if (Mathf.Abs(x) > 0.25f && isOnGround) animator.SetBool("isSkipping", true);
         else animator.SetBool("isSkipping", false);
@@ -49,7 +54,10 @@ public class Player : MonoBehaviour
 
     void FixedUpdate() //물리법칙과 관련된 것들을 연속적으로 사용할 때 넣어주자(rigidbody와 관련된 것 등)
     {
-        rigid.velocity = new Vector2(x * 200f * Time.deltaTime, rigid.velocity.y);
+       if(!isDamaged)
+        {
+            rigid.velocity = new Vector2(x * 200f * Time.deltaTime, rigid.velocity.y);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -60,7 +68,7 @@ public class Player : MonoBehaviour
             animator.SetBool("isOnGround", true);
         }
         //Star먹음
-        if(collision.gameObject.tag == "Star")
+        if (collision.gameObject.tag == "Star")
         {
             Destroy(collision.gameObject);
             star.Play();
@@ -74,5 +82,54 @@ public class Player : MonoBehaviour
             isOnGround = false;
             animator.SetBool("isOnGround", false);
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //Attack
+            if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
+            {
+                OnAttack(collision.transform);
+            }
+            else  //Damaged 
+                OnDamaged(collision.transform.position);
+            
+        }
+    }
+
+    void OnAttack(Transform enemy)
+    {
+        //Point
+
+        //Enemy Die
+        EnemyMove enemyMove = enemy.GetComponent<EnemyMove>();
+        enemyMove.OnDamaged();
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        isDamaged = true;
+
+        gameObject.layer = 10;
+
+        sr.color = new Color(1, 1, 1, 0.6f);
+
+        //Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1) * 3, ForceMode2D.Impulse);
+
+        //Animation
+        animator.SetTrigger("damaged");
+
+        Invoke("OffDamaged", 1);
+    }
+
+    void OffDamaged()
+    {
+        isDamaged = false;
+        gameObject.layer = 9;
+        sr.color = new Color(1, 1, 1, 1);
     }
 }
